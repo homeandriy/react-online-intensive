205bce4ff77d4bd81c6bb7a5429a023bd63c769b
homeandriy <homeandriy@gmail.com>
2018-10-22 21:49:54 +0300

    And Lesson 5? немає домашки

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -4,18 +4,31 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 // instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts:[
+            { id : '123', comment : 'Hy Thea', created: 1526825076849 }, 
+            { id : '153', comment : 'Hy two Thea', created: 1526825076851 }
+        ]
+    }
     render () {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post }/>; 
+        });
         return (
             <section className= { Styles.feed }>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post /> 
+                { postsJSX }
             </section>  
         );
     }
-}
+} 
