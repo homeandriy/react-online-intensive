d537d24b09d422d33820309a60d9cdc4aae76eea
andrii.beznosko <andrii.beznosko@volia.com>
2018-10-22 17:41:37 +0300

    End 3 lesson

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,16 +1,18 @@
+//Core
 import React, { Component } from 'react';
 
-
-import avatar from 'theme/assets/lisa';
+//Instruments
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
     render () {
+        const  {avatar, currentUserFirstName } = this.props;
+
         return (
             <section className= { Styles.composer }>
                 <img src= { avatar } />
                 <form>
-                    <textarea placeholder = { `What's on your mind, Lisa?` } />
+                    <textarea placeholder = { `What's on your mind, ${ currentUserFirstName }?` } />
                     <input type='submit' value='Post' />   
                 </form>
             </section>
