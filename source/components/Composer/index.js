59c33965c029e9fa3f020d1ac8053aaf69f3d4dc
homeandriy <homeandriy@gmail.com>
2018-10-22 22:45:36 +0300

    End lesson 6.1 nt enter button

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,19 +1,62 @@
 //Core
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 //Instruments
 import { Consumer } from 'components/HOC/withProfile';
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
+    static PropTypes = {
+        _createPost: PropTypes.func.isRequired,
+    };
+
+    constructor() {
+        super();
+
+        this._updateComment = this._updateComment.bind(this);
+        this._submitComment = this._submitComment.bind(this);
+    }    
+
+    state = {
+        comment: '',
+    };
+
+    _updateComment (event) {
+        this.setState({
+            comment: event.target.value,
+        });
+    }
+
+    _submitComment ( event ) {
+        event.preventDefault();
+        const { comment } = this.state;
+
+        if( !comment ) {
+            return null;
+        }
+
+        this.props._createPost( comment );
+
+        this.setState({
+            comment: '',
+        });
+    }
+
     render () {
+        const { comment } = this.state;
+        
         return (
             <Consumer>
                 {(context)=>(
                     <section className= { Styles.composer }>
                     <img src= { context.avatar } />
-                    <form>
-                        <textarea placeholder = { `What's on your mind, ${ context.currentUserFirstName }?` } />
+                    <form onSubmit = { this._submitComment }>
+                        <textarea 
+                            placeholder = { `What's on your mind, ${ context.currentUserFirstName }?` }
+                            value = { comment }
+                            onChange = { this._updateComment } 
+                            />
                         <input type='submit' value='Post' />   
                     </form>
                 </section>
--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,4 +1,6 @@
+//Core
 import React, { Component } from 'react';
+import moment from 'moment';
 
 //Components
 import StatusBar from 'components/StatusBar';
@@ -8,25 +10,45 @@ import Spinner from 'components/Spinner';
 
 // instruments
 import Styles from './styles.m.css';
+import { getUniqueID } from 'instruments';
 
 export default class Feed extends Component {
+    constructor() {
+        super();
+
+        this._createPost = this._createPost.bind(this);
+    }
+
     state = {
         posts:[
             { id : '123', comment : 'Hy Thea', created: 1526825076849 }, 
             { id : '153', comment : 'Hy two Thea', created: 1526825076851 }
-        ]
+        ],
+        isPostsFetching: false,
     }
+
+    _createPost(comment) {
+        const post = {
+            id:      getUniqueID(),
+            created: moment.now(),
+            comment,
+        }
+        this.setState(({ posts }) => ({
+            posts: [post, ...posts],
+        }));
+    }
+
     render () {
-        const { posts } = this.state;
+        const { posts, isPostsFetching } = this.state;
 
         const postsJSX = posts.map((post) => {
             return <Post key = { post.id } { ...post }/>; 
         });
         return (
             <section className= { Styles.feed }>
-                <Spinner isSpinning />
+                <Spinner isSpinning = { isPostsFetching } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 { postsJSX }
             </section>  
         );
