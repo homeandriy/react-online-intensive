a06f90c34b20b334027b3c399132c99e03cb4749
andrii.beznosko <andrii.beznosko@volia.com>
2018-10-22 15:43:54 +0300

    lesson 1

---
+++ source/components/Composer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+import avatar from 'theme/assets/lisa';
+
+export default class Post extends Component {
+    render () {
+        return (
+            <section>              
+                <img src = { avatar } />
+                <a>Lisa Simpson </a>
+                <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                <p>Howdy</p>
+            </section>  
+        );
+    }
+}
